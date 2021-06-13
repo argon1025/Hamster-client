fb8aa7ba2065c3158fbd8c3167b8561e747e7d99
조민제 <chominje@jominje-ui-MacBookAir.local>
2021-06-13 14:40:09 +0900

    소켓 서버 변경, 일부 펑션 수정

--- module/Client.ts
+++ module/Client.ts
@@ -13,7 +13,7 @@ class Client implements iClient {
   isconnect: boolean;
 
   public connectSocket: () => void = async () => {
-    let socket = io.connect("http://localhost:8080", {
+    let socket = io.connect("http://ec2-13-125-208-218.ap-northeast-2.compute.amazonaws.com:8828", {
       reconnectionAttempt: 3,
       // reconnection: false,
       // autoConnect: false,
@@ -37,28 +37,28 @@ class Client implements iClient {
     socket.on("connect", () => {
       console.log("conenc");
       // 1. 클라이언트가 연결되었을때 대쉬에게 전달한 정보 emit (client_setClientInfo)
-      socket.emit("client_setClientInfo", ip.address());
+      socket.emit("client_newClient", ip.address());
     });
     // 2. 대쉬보드가 요청 했을때 클라이언트의 정보 알려주기 on (client_getClientInfo)
-    socket.on("client_getClientInfo", ()=>{
-      console.log("back clientinfo");
-      socket.emit("client_setClientInfo", ip.address());
+    socket.on("client_getClientInfo", (dash_socketID)=>{
+      console.log("i'm joined", dash_socketID);
+      socket.emit("client_setClientInfo", ip.address(), dash_socketID);
     })
-    socket.on("shutdown", async () => {
+    socket.on("shutdown", async ( dashboardID) => {
       const result = await commandRun("shutdown -s -t 10");
-      socket.emit("logEvent", result);
+      socket.emit("client_logEvent",dashboardID, ip.address(), socket.id, result);
     });
-    socket.on("reboot", async () => {
+    socket.on("reboot", async ( dashboardID) => {
       const result = await commandRun("shutdown -r -t 10");
-      socket.emit("logEvent", result);
+      socket.emit("client_logEvent",dashboardID, ip.address(), socket.id, result);
     });
-    socket.on("commnand", async (command) => {
-      const result = await commandRun(command);
-      socket.emit("logEvent", result);
+    socket.on("commnand", async ( dashboardID, data ) => {
+      const result = await commandRun(data);
+      socket.emit("client_logEvent",dashboardID, ip.address(), socket.id, result);
     });
-    socket.on("filedown", async (url) => {
-      const result = await download(url, `${process.env.APPDATA}\\download`);
-      socket.emit("logEvent", result);
+    socket.on("filedown", async ( dashboardID, data ) => {
+      const result = await download(data, `${process.env.APPDATA}\\download`);
+      socket.emit("client_logEvent",dashboardID, ip.address(), socket.id, result);
     });
   };
 }
